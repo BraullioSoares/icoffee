8106226aca7cd04d4b30a2de10264d7fe0624705
Braullio Soares <braulliosoares.bs@gmail.com>
2018-11-15 01:20:47 -0300

    [DEV-WEB] - iCoffee.com Implrment pagination and forms

--- frontend/app/billingCycle/billilngCycleController.js
+++ frontend/app/billingCycle/billilngCycleController.js
@@ -2,21 +2,28 @@
 
     angular.module('iCoffeeApp').controller('BillingCycleCtrl', [
         '$http',
+        '$location',
         'consts',
         'msgs',
         'tabs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http, consts, msgs, tabs) {
+    function BillingCycleController($http, $location, consts, msgs, tabs) {
         const vm = this
         const url = `${consts.apiUrl}/billingCycles`
 
         vm.refresh = function() {
-            $http.get(url).then(function(response) {
+            const page = parseInt($location.search().page) || 1
+            $http.get(`${url}?skip=${(page - 1) * 10}&liimit=10`).then(function(response) {
                 vm.billingCycle = {credits: [{}], debts: [{}]}
                 vm.billingCycles = response.data
-                tabs.show(vm, {tabList: true, tabCreate: true})
+                vm.calculateValues()
+                
+                $http.get(`${url}/count`).then(function(response) {
+                    vm.pages = Math.ceil(response.data.value / 10)
+                    tabs.show(vm, {tabList: true, tabCreate: true})
+                })
             })
         }
 
@@ -31,16 +38,18 @@
 
         vm.showTabUpdate = function(billingCycle) {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, {tabUpdate: true})
         }
 
         vm.showTabDelete = function(billingCycle) {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, {tabDelete: true})
         }
 
         vm.update = function() {
-            const updateUrl = `${consts.apiUrl}/billingCycles/${vm.billingCycle._id}`
+            const updateUrl = `${url}/${vm.billingCycle._id}`
             $http.put(updateUrl, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação Efetuada com Sucesso!')
@@ -50,7 +59,7 @@
         }
 
         vm.delete = function() {
-            const deleteUrl = `${consts.apiUrl}/billingCycles/${vm.billingCycle._id}`
+            const deleteUrl = `${url}/${vm.billingCycle._id}`
             $http.delete(deleteUrl, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação Efetuada com Sucesso!')
@@ -59,7 +68,54 @@
             })
         }
 
+        vm.addCredit = function(index) {
+            vm.billingCycle.credits.splice(index + 1, 0, {})
+        }
 
+        vm.cloneCredit = function(index, {name, value}) {
+            vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+            vm.calculateValues()
+        }
+
+        vm.deleteCredit = function(index) {
+            if (vm.billingCycle.credits.length > 1 ) {
+                vm.billingCycle.credits.splice(index, 1)
+                vm.calculateValues()
+            }
+        }
+
+        vm.addDebt = function(index) {
+            vm.billingCycle.debts.splice(index + 1, 0, {})
+        }
+
+        vm.cloneDebt = function(index, {name, value, status}) {
+            vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+            vm.calculateValues()
+        }
+
+        vm.deleteDebt = function(index) {
+            if (vm.billingCycle.debts.length > 1 ) {
+                vm.billingCycle.debts.splice(index, 1)
+                vm.calculateValues()
+            }
+        }
+
+        vm.calculateValues = function() {
+            vm.credit = 0
+            vm.debt = 0
+
+            if (vm.billingCycle) {
+                vm.billingCycle.credits.forEach(({value}) => {
+                    vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+                });
+
+                vm.billingCycle.debts.forEach(({value}) => {
+                    vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+                });
+            }
+
+            vm.total = vm.credit - vm.debt
+        }
         vm.refresh()
     }
 }())
--- frontend/app/config/routes.js
+++ frontend/app/config/routes.js
@@ -6,7 +6,7 @@ angular.module('iCoffeeApp').config([
             url: "/dashboard",
             templateUrl: "dashboard/dashboard.html"
         }).state('billingCycle', {
-            url: "/billingCycle",
+            url: "/billingCycle?page",
             templateUrl: "billingCycle/tabs.html"
         })
 
